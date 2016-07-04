40c2c43e52458329cf6b256389f794b93c32544d
Sathish Guru <sathishgurucse@gmail.com>
2016-07-04 14:25:32 +0530

    Update utils.py

--- flask_report/utils.py
+++ flask_report/utils.py
@@ -72,7 +72,7 @@ def query_to_sql(statement, bind=None):
     if isinstance(statement, sqlalchemy.orm.Query):
         if bind is None:
             bind = statement.session.get_bind(
-                statement._mapper_zero_or_none()
+                statement._mapper_zero()
             )
         statement = statement.statement
     elif bind is None:
